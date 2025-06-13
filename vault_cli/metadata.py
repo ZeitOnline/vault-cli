58ebe9a4e2013554c24681a24dd09acb174fea41
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-06-13 14:44:58 +0200

    Replace deprecated pkg_resources with stdlib importlib.metadata

--- vault_cli/metadata.py
+++ vault_cli/metadata.py
@@ -1,21 +1,16 @@
-import io
-from distutils import dist
+import importlib.metadata
 from typing import Mapping, Optional
 
-import pkg_resources
 
 
 def extract_metadata() -> Mapping[str, Optional[str]]:
 
-    distribution = pkg_resources.get_distribution("vault-cli")
-    metadata_str = distribution.get_metadata(distribution.PKG_INFO)
-    metadata_obj = dist.DistributionMetadata()
-    metadata_obj.read_pkg_file(io.StringIO(metadata_str))
+    metadata_obj = importlib.metadata.metadata("vault-cli")
 
     return {
-        "author": metadata_obj.author,
-        "email": metadata_obj.author_email,
-        "license": metadata_obj.license,
-        "url": metadata_obj.url,
-        "version": metadata_obj.version,
+        "author": metadata_obj["author"],
+        "email": metadata_obj["author-email"],
+        "license": metadata_obj["license"],
+        "url": metadata_obj["home-page"],
+        "version": metadata_obj["version"],
     }
